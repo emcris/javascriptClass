e63bac358a77206e7415f680f498eae220943f3a
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-14 18:34:34 -0700

    Linked JS, first look at console log

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var  firstName = 'emily';
+var  age = 22;
+
+//Old Way
+
+console.log("My name is " + firstName + ' and my age is ' + age);
+
+//Better Way
+//WRITE IT LIKE THIS WITH BACKTICKS ``
+
+console.log(`My name is ${firstName} and my name is ${age}`)
