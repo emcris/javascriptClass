db9e3197ca62ba926339a9a54e7eaad46c99db25
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-14 19:03:21 -0700

    all the stuff we got to at our break

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-var  firstName = 'emily';
-var  age = 22;
+// var  firstName = 'emily';
+// var  age = 22;
+
+// const firstName = 'Emily';
+// firstName = 'Cris';
 
 //Old Way
 
-console.log("My name is " + firstName + ' and my age is ' + age);
+//console.log("My name is " + firstName + ' and my age is ' + age);
 
 //Better Way
 //WRITE IT LIKE THIS WITH BACKTICKS ``
 
-console.log(`My name is ${firstName} and my name is ${age}`)
+//console.log(`My name is ${firstName} and my name is ${age}`);
+
+
+//alert(`Hello I am ${firstName}`);
+
+// const birthday = '05/08/1997';
+// console.log(birthday);
+// let fixDate = new Date();
+// console.log(fixDate);
+
+let hello = new String('Hello, there.');
+alert(hello);
