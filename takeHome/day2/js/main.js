3597597292ed3cc161d725416c09aecfb266aa64
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-14 20:30:25 -0700

    finished birthday alert with new Date

--- takeHome/day2/js/main.js
+++ takeHome/day2/js/main.js
@@ -4,3 +4,6 @@ console.log(firstName);
 let lastName = prompt('What is your last name?');
 alert(`Is this your last name: ${lastName}`)
 
+let birthday = prompt('What is your birthday?');
+confirm(`Is this your correct birthday? ${new Date (birthday)}`);
+alert(`Your birthdy ${new Date (birthday)} has been saved!`);
