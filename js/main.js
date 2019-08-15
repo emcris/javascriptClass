c16357deb40ce6aed1b69a76c375e5f0c5fe228a
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-14 19:44:44 -0700

    Arrays and stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 // var  firstName = 'emily';
 // var  age = 22;
 
-// const firstName = 'Emily';
+const firstName = 'Emily';
+const areYouMarried = false;
+
+console.log(firstName.length);
+
+// const question = prompt('Are you married');
+
 // firstName = 'Cris';
 
 //Old Way
@@ -21,5 +27,9 @@
 // let fixDate = new Date();
 // console.log(fixDate);
 
-let hello = new String('Hello, there.');
-alert(hello);
+// console.log(typeof{});
+
+const topThreeMovies = ['Lion King', 'End Game', 'Anastasia']
+console.log(topThreeMovies);
+console.log(`My top three movies are: ${topThreeMovies}`);
+console.log(`my second favorite movie is: ${topThreeMovies[1]} and the type is: ${typeof topThreeMovies}`);
