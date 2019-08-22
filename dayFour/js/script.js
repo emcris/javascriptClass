4379eff2a844aafad328cbacf1d60a53204a449f
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-21 18:14:59 -0700

    if and else if statements

---
+++ dayFour/js/script.js
@@ -0,0 +1,12 @@
+//if and else/if statements
+const greeting = 'hello';
+
+if (greeting === 'hi') {
+    console.log('yes, this matches to HI');
+} else if (greeting === 'hello') {
+    console.log('yes, this matched HELLO');
+} else if (greeting === 1) {
+    console.log('yes, this matches and integer of 1');
+} else {
+    console.log('none of these matched.');
+}
