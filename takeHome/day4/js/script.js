91b0a7b266f8838b016c8f33e119d72480dce8cf
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-26 15:43:42 -0700

    todo let set up

---
+++ takeHome/day4/js/script.js
@@ -0,0 +1,10 @@
+let todo1 = prompt('Input 1 to-do you have for this weekend. (1/3)');
+let todo2 = prompt('Input 1 to-do you have for this weekend. (2/3)');
+let todo3 = prompt('Input 1 to-do you have for this weekend. (3/3)');
+
+let todos = [
+    todo1,
+    todo2,
+    todo3
+];
+
