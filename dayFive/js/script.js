4fdcc7c046c0e9fb73e2aa05b695a2e0fa14e585
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-26 19:07:48 -0700

    functions

---
+++ dayFive/js/script.js
@@ -0,0 +1,17 @@
+//anon function
+
+let example = function () {
+    console.log('hello,there');
+}
+
+//naming function
+function exampleName() {
+    console.log('hello,there');
+}
+
+//example();
+//exampleName();
+
+let turnValueUppercase = val => console.log(val.toUpperCase());
+
+turnValueUppercase('hi');
