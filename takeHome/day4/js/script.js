663d4a86da2b8723e17de0a8c5161847b89c7eb1
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-26 18:40:48 -0700

    last commit

--- takeHome/day4/js/script.js
+++ takeHome/day4/js/script.js
@@ -1,45 +1,47 @@
 //creat 3 variables of prompt values
-const taskOne = prompt('Input 1 to-do you have for this weekend. (1/3)');
-const taskTwo = prompt('Input 1 to-do you have for this weekend. (2/3)');
-const taskThree = prompt('Input 1 to-do you have for this weekend. (3/3)');
+// const taskOne = prompt('Input 1 to-do you have for this weekend. (1/3)');
+// const taskTwo = prompt('Input 1 to-do you have for this weekend. (2/3)');
+// const taskThree = prompt('Input 1 to-do you have for this weekend. (3/3)');
 
-let arrayOfTasks = [
-    taskOne,
-    taskTwo,
-    taskThree
-];
+// let arrayOfTasks = [
+//     taskOne,
+//     taskTwo,
+//     taskThree
+// ];
 
 //parent and child array
-let secondArrayOfTasks = [
-    [taskOne],
-    [taskTwo],
-    [taskThree]
-];
+// let secondArrayOfTasks = [
+//     [taskOne],
+//     [taskTwo],
+//     [taskThree]
+// ];
 
-console.log(arrayOfTasks);
-console.log('second one',secondArrayOfTasks);
+// console.log(arrayOfTasks);
+// console.log('second one',secondArrayOfTasks);
 
-const limit = 3;
-for(j = 0; j < limit; j++) {
-    secondArrayOfTasks[j].push(`this will take ${Math.floor(Math.random() * 5)}`);
-    console.log(secondArrayOfTasks[j]);
-}
+// const limit = 3;
+// for(j = 0; j < limit; j++) {
+//     secondArrayOfTasks[j].push(`this will take ${Math.floor(Math.random() * 5)}`);
+//     console.log(secondArrayOfTasks[j]);
+// }
 
 //this is michales
-let numberOfItems = 3;
-let toDoList = [];
+// let numberOfItems = 3;
+// let toDoList = [];
 
-for(let i = 0; i < numberOfItems; ++i) {
-    toDoList.push([prompt(`Enter todo Item# ${i+1}`)]);
-}
+// for(let i = 0; i < numberOfItems; ++i) {
+//     toDoList.push([prompt(`Enter todo Item# ${i+1}`)]);
+// }
 
-let longestTaskName = toDoList[0][0];
-let longestTaskDays = toDoList[0][1];
-let indicator = 0;
+// let longestTaskName = toDoList[0][0];
+// let longestTaskDays = toDoList[0][1];
+// let indicator = 0;
 
-while(indicator < toDoList.length) {
-    if(toDoList[indicator][1]>longestTaskDays) {
-        longestTaskName = toDoList[indicator][0];
-        longestTaskDays = toDoList
-    }
-}
+// while(indicator < toDoList.length) {
+//     if(toDoList[indicator][1]>longestTaskDays) {
+//         longestTaskName = toDoList[indicator][0];
+//         longestTaskDays = toDoList
+//     }
+// }
+//will be on slack
+
