cc9e60b1f4d4cf35ca3f1cdedb44f1d8c7a0895d
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-09-02 19:39:44 -0700

    list item in js

--- daySeven/js/script.js
+++ daySeven/js/script.js
@@ -1,4 +1,25 @@
+const container = document.querySelector('.container');
+console.log(container);
+
 const h1 = document.querySelector('h1');
-console.log(h1);
-let message = h1.innerHTML = 'I am a message';
-//console.log(h1.textContent);
+
+function globalListItems() {
+    //create a ul/ol, create li with  any class, attributes or ids needed 
+
+    //create ul
+    const ul = document.createElement('ul');
+    console.log('unordered list', ul);
+    //create a li
+    const li = document.createElement('li');
+    console.log('list item', li);
+    //attributes/classes/id
+    li.className = 'list-group-item list-group-item-info';
+    console.log('li, with classes', li);
+    li.innerHTML = 'i am a list item';
+    ul.appendChild(li);
+    console.log('complete ul', ul);
+    container.appendChild(ul);
+    console.log('complete ul in container', ul);
+}
+
+globalListItems();
