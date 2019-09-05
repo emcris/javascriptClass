087fb3c71270582a1c8dd427829405abc58930f2
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-09-04 20:16:40 -0700

    my form

--- dayEight/js/script.js
+++ dayEight/js/script.js
@@ -26,29 +26,56 @@ content.appendChild(form);
 const email = document.createElement('input');
 email.setAttribute('name', 'email');
 email.setAttribute('type', 'email');
-//email.setAttribute('placeholder', 'email@email.com');
+email.setAttribute('placeholder', 'youremail@email.com');
+email.setAttribute('class', 'form-control specialElements');
 form.appendChild(email);
 
 const subject = document.createElement('input');
 subject.setAttribute('name', 'subject');
 subject.setAttribute('type', 'text');
+subject.setAttribute('placeholder', 'Subject');
+subject.setAttribute('class', 'form-control specialElements');
 form.appendChild(subject);
 
 const textArea = document.createElement('input');
 textArea.setAttribute('type', 'textarea');
-textArea.setAttribute('type', 'text');
+textArea.setAttribute('type', 'textarea');
+textArea.setAttribute('class', 'form-control specialElements');
+textArea.setAttribute('placeholder', 'Type your message here ...');
 form.appendChild(textArea);
 
 const submitButton = document.createElement('button');
 submitButton.setAttribute('type', 'submit');
-//submitButton.setAttribute('placeholder', 'submit');
+submitButton.setAttribute('text', 'submit');
+submitButton.className = 'btn-primary btn specialElements';
+submitButton.innerText = 'submit';
 
 
 //email.setAttribute = 'type', 'text';
 form.appendChild(submitButton);
 
-let emailForm = document.forms.sendEmail;
-emailForm.elements.email.placeholder = 'example@emial.com';
-emailForm.elements.subject.placeholder = 'subject';
-emailForm.elements.textarea.placeholder = 'Type message here ...';
-emailForm.elements.submit.placeholder = 'Submit';
+// let emailForm = document.forms.sendEmail;
+
+function globalFormElements(element, elemntType, elementName, elementClass, elementPlaceholder){
+    //element : create the type of element you seek
+    const newElement = document.createElement(element);
+    newElement.setAttribute('type', elemntType);
+    newElement.className = elementClass;
+    newElement.setAttribute('name', elementName);
+    newElement.setAttribute('placeholder', elementPlaceholder);
+    return element;
+}
+const testAreaElement = (name, placeholder, elementClass, elementId, cols = 4, rows = 2)  => {
+    const element = document.createElement('textarea');
+    element.setAttribute('type', 'text');
+    element.className = elementClass;
+    element.setAttribute('name', name);
+    element.setAttribute('placeholder', placeholder);
+    element.setAttribute('cols', cols);
+    element.setAttribute('rows', rows);
+    element.id = elementId;
+
+}
+console.log(globalFormElements('input', 'text', 'example', 'form-control bg-primary', 'Type your example here'));
+
+console.log(globalFormElements('textarea', 'text', 'message', 'bg-danger', 'Please, let us know your concerns here.'));
