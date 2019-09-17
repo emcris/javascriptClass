1c35f89dab1bc38e5c6a57ce62cdaf6a17c504ff
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-09-16 20:14:43 -0700

    api practice

---
+++ dayEleven/js/script.js
@@ -0,0 +1,60 @@
+//https://dog.ceo/api/breeds/image/random
+function playWithApi() {
+    const url = 'https://dog.ceo/api/breeds/list/all';
+    fetch(url);
+    console.log(url);
+}
+
+playWithApi();
+
+// const box = document.createElement('div');
+// const button = document.createElement('button');
+// button.setAttribute('href', 'url');
+// box.appendChild(button);
+// button.addEventListener('click', (getUrl) => {
+
+// });
+
+//Playing with Json
+// const exampleJson = '{ "name ": "Emily", "age": 22}';
+// const exapmleParse = JSON.parse(exapmleParse);
+// //wrap.innerText = exampleJson.name;
+// wrap.innerText = exapmleParse.name;
+// const exampleStringify = JSON.stringify(exampleTwo);
+// wrapper.innerText = exampleStringify;
+// console.log(exampleJson);
+
+//closure
+function practiceScope() {
+    let random = 'Inside scope.';
+
+    const phrase = function () {
+        console.log('this is super cool, jax');
+    }
+
+    return phrase();
+}
+
+let practiceOutside = practiceScope();
+
+class aboutMe {
+    constructor(name, age, petName, petBreed) {
+        this.name = name;
+        this.age = age;
+        this.petName = petName;
+        this.petBreed = petBreed;
+    }
+        
+}
+
+aboutMe.prototype.myDog = fuction() {
+    const url = `https://dog.ceo/api/${this.petBreed}/list/all`
+    fetch(url);
+    console.log(url);
+
+}
+    
+
+const emily = new aboutMe('Emily', 22, 'Ruby', 'boxer')
+console.log(emily);
+
